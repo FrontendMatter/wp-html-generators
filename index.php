14ea814878787335e158269e342816c1e1822ce4
Laza Bogdan <laza.bogdan@macro-net.ro>
2014-04-09 14:51:13 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: WP Composer Package
+Plugin URI: http://mosaicpro.biz
+Description: WordPress Plugin as Composer package
+Version: 1.0
+Author: MosaicPro
+Author URI: http://mosaicpro.biz
+*/
