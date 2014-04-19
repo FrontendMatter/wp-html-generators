35b3917c4ec2667379c3d4c6a68ee968648714c5
Laza Bogdan <laza.bogdan@macro-net.ro>
2014-04-19 11:44:22 +0300

    removed the WP init action;

--- index.php
+++ index.php
@@ -8,91 +8,87 @@ Author: MosaicPro
 Author URI: http://mosaicpro.biz
 */
 
-add_action('init', function()
+// include the autoloader
+include dirname(__FILE__) . '/vendor/autoload.php';
+
+// include the illuminate/support helpers
+require_once dirname(__FILE__) . '/vendor/illuminate/support/Illuminate/Support/helpers.php';
+
+// instantiate the container
+$app = new \Illuminate\Container\Container;
+$request = new \Illuminate\Http\Request;
+$app->bind('app', $app);
+$app->bind('request', $request);
+$app->bind('env', function(){});
+$app->bind('session.store', function(){});
+
+// define the providers
+$providers = array(
+    'Illuminate\Events\EventServiceProvider',
+    'Illuminate\Routing\RoutingServiceProvider',
+);
+
+// Register the providers
+$registered = [];
+foreach ($providers as $provider) {
+    $instance = new $provider($app);
+    $instance->register();
+    $registered[] = $instance;
+}
+
+// Boot the providers
+foreach ($registered as $instance)
+    $instance->boot();
+
+// bind the html builder to the container
+$app->bindShared('html', function($app)
 {
-    // include the autoloader
-    include dirname(__FILE__) . '/vendor/autoload.php';
-
-    // include the illuminate/support helpers
-    require_once dirname(__FILE__) . '/vendor/illuminate/support/Illuminate/Support/helpers.php';
-
-    // instantiate the container
-    $app = new \Illuminate\Container\Container;
-    $request = new \Illuminate\Http\Request;
-    $app->bind('app', $app);
-    $app->bind('request', $request);
-    $app->bind('env', function(){});
-    $app->bind('session.store', function(){});
-
-    // define the providers
-    $providers = array(
-        'Illuminate\Events\EventServiceProvider',
-        'Illuminate\Routing\RoutingServiceProvider',
-    );
-
-    // Register the providers
-    $registered = [];
-    foreach ($providers as $provider) {
-        $instance = new $provider($app);
-        $instance->register();
-        $registered[] = $instance;
-    }
-
-    // Boot the providers
-    foreach ($registered as $instance)
-        $instance->boot();
-
-    // bind the html builder to the container
-    $app->bindShared('html', function($app)
-    {
-        return new \Illuminate\Html\HtmlBuilder($app['url']);
-    });
-
-    // bind the form builder to the container
-    $app->bindShared('form', function($app)
-    {
-        $form = new \Illuminate\Html\FormBuilder($app['html'], $app['url'], '');
-        return $form;
-    });
-
-    // bind the config component to the container
-    $app->bindShared('config', function($app)
-    {
-        $configPath = __DIR__ . '/../config';
-        $environment = 'production';
-
-        $file = new \Illuminate\Filesystem\Filesystem;
-        $loader = new \Illuminate\Config\FileLoader($file, $configPath);
-        $config = new \Illuminate\Config\Repository($loader, $environment);
-        return $config;
-    });
-
-    // register mosaicpro/form config package
-    $app['config']->package('mosaicpro/form', __DIR__ . '/vendor/mosaicpro/form/src/config');
-
-    // register the container
-    \Mosaicpro\Core\IoC::setContainer($app);
-
-    // create shorter aliases for the components
-    class_alias('Mosaicpro\Accordion\Accordion', 'Accordion');
-    class_alias('Mosaicpro\Alert\Alert', 'Alert');
-    class_alias('Mosaicpro\Button\Button', 'Button');
-    class_alias('Mosaicpro\ButtonGroup\ButtonGroup', 'ButtonGroup');
-    class_alias('Mosaicpro\Carousel\Carousel', 'Carousel');
-    class_alias('Mosaicpro\Dropdown\Dropdown', 'Dropdown');
-    class_alias('Mosaicpro\Form\FormField', 'FormField');
-    class_alias('Mosaicpro\Form\Checkbox', 'Checkbox');
-    class_alias('Mosaicpro\ListGroup\ListGroup', 'ListGroup');
-    class_alias('Mosaicpro\Media\Media', 'Media');
-    class_alias('Mosaicpro\Modal\Modal', 'Modal');
-    class_alias('Mosaicpro\Nav\Nav', 'Nav');
-    class_alias('Mosaicpro\Navbar\Navbar', 'Navbar');
-    class_alias('Mosaicpro\Panel\Panel', 'Panel');
-    class_alias('Mosaicpro\ProgressBar\ProgressBar', 'ProgressBar');
-    class_alias('Mosaicpro\Tab\Tab', 'Tab');
-    class_alias('Mosaicpro\Table\Table', 'Table');
-
+    return new \Illuminate\Html\HtmlBuilder($app['url']);
 });
+
+// bind the form builder to the container
+$app->bindShared('form', function($app)
+{
+    $form = new \Illuminate\Html\FormBuilder($app['html'], $app['url'], '');
+    return $form;
+});
+
+// bind the config component to the container
+$app->bindShared('config', function($app)
+{
+    $configPath = __DIR__ . '/../config';
+    $environment = 'production';
+
+    $file = new \Illuminate\Filesystem\Filesystem;
+    $loader = new \Illuminate\Config\FileLoader($file, $configPath);
+    $config = new \Illuminate\Config\Repository($loader, $environment);
+    return $config;
+});
+
+// register mosaicpro/form config package
+$app['config']->package('mosaicpro/form', __DIR__ . '/vendor/mosaicpro/form/src/config');
+
+// register the container
+\Mosaicpro\Core\IoC::setContainer($app);
+
+// create shorter aliases for the components
+class_alias('Mosaicpro\Accordion\Accordion', 'Accordion');
+class_alias('Mosaicpro\Alert\Alert', 'Alert');
+class_alias('Mosaicpro\Button\Button', 'Button');
+class_alias('Mosaicpro\ButtonGroup\ButtonGroup', 'ButtonGroup');
+class_alias('Mosaicpro\Carousel\Carousel', 'Carousel');
+class_alias('Mosaicpro\Dropdown\Dropdown', 'Dropdown');
+class_alias('Mosaicpro\Form\FormField', 'FormField');
+class_alias('Mosaicpro\Form\Checkbox', 'Checkbox');
+class_alias('Mosaicpro\ListGroup\ListGroup', 'ListGroup');
+class_alias('Mosaicpro\Media\Media', 'Media');
+class_alias('Mosaicpro\Modal\Modal', 'Modal');
+class_alias('Mosaicpro\Nav\Nav', 'Nav');
+class_alias('Mosaicpro\Navbar\Navbar', 'Navbar');
+class_alias('Mosaicpro\Panel\Panel', 'Panel');
+class_alias('Mosaicpro\ProgressBar\ProgressBar', 'ProgressBar');
+class_alias('Mosaicpro\Tab\Tab', 'Tab');
+class_alias('Mosaicpro\Table\Table', 'Table');
 
 add_action('admin_enqueue_scripts', function()
 {
